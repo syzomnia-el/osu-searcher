4926616045adc2ccbe327f3d7bddb8d7fc5b8587
syzomnia-el <sagachinjufu@gmail.com>
2023-12-10 22:46:49 +0800

    refactor: refactor test.utils package for timer

--- test/utils/__init__.py
+++ test/utils/__init__.py
@@ -1,92 +1,84 @@
 # -*- coding: utf-8 -*-
 import timeit
-from multiprocessing import Process, Queue
+from concurrent.futures import ProcessPoolExecutor
+from dataclasses import dataclass
 from statistics import mean
 from typing import Callable
 
+DEFAULT_REPEAT: int = 10
+DEFAULT_NUMBER: int = 1000
 
-def timer(func: Callable, *args, repeat: int = 10, number: int = 1000) -> float:
+type Function = Callable[[], None]
+type TimerWrapperFunction = Callable[[Function], TimerResult]
+
+
+@dataclass(frozen=True)
+class TimerResult:
+    """
+    The timer result type.
+
+    Attributes:
+        func: The function to be measured.
+        time: The average execution time of the function in milliseconds.
+    """
+    func: Function
+    time: float
+
+
+def timer(func: Function, repeat: int = DEFAULT_REPEAT, number: int = DEFAULT_NUMBER) -> TimerResult:
     """
     Measures the average execution time of a function.
 
-    Parameters:
-        func: The function to be measured.
-        *args: The arguments of the function.
-        repeat: The times number to repeat the measurement.
-        number: The times number to execute the function.
-
-    Returns:
-        The average execution time of the function in milliseconds.
+    :param func: The function to be measured.
+    :param repeat: The times number to repeat the measurement.
+    :param number: The times number to execute the function.
+    :return: The average execution time of the function in milliseconds.
     """
+    global DEFAULT_REPEAT, DEFAULT_NUMBER
     if not callable(func):
-        raise TypeError(f'{func.__name__} is not callable.')
-    if not isinstance(repeat, int):
-        raise TypeError(f'{repeat=} is not an integer.')
-    if not isinstance(number, int):
-        raise TypeError(f'{number=} is not an integer.')
-    if repeat <= 0:
-        raise ValueError(f'{repeat=} is invalid.')
-    if number <= 0:
-        raise ValueError(f'{number=} is invalid.')
+        raise TypeError('func must be a callable object.')
+    if not isinstance(repeat, int) or repeat <= 0:
+        raise TypeError('repeat must be a positive integer.')
+    if not isinstance(number, int) or number <= 0:
+        raise TypeError('number must be a positive integer.')
 
-    args_str = None
-    if args:
-        args_str = ', '.join(*args) if len(*args) > 1 else str(*args)
-
-    class_name = func.__qualname__.split('.')[0]
-
-    stmt = f'{func.__qualname__}({args_str})' if args_str else f'{func.__qualname__}()'
-    setup = f'from __main__ import {class_name}'
-
-    return mean(timeit.repeat(stmt=stmt, setup=setup, repeat=repeat, number=number)) * 1000
+    print(f'Measuring the execution time of {func.__name__}...')
+    times = timeit.repeat(func, repeat=repeat, number=number)
+    print(f'Measurement completed of {func.__name__}, the average execution time is {mean(times):.6f} ms')
+    return TimerResult(func=func, time=mean(times))
 
 
-def _get_timer_result(queue: Queue, func: Callable, *args, repeat: int = 10, number: int = 1000) -> None:
+def default_timer_wrapper(func: Function) -> TimerResult:
     """
-    Gets the result of timing the execution of a function.
+    The default timer wrapper function.
 
-    Args:
-        queue: A queue to store the timer result.
-        func: The function to be measured.
-        *args: The arguments of the function.
-        repeat: The times number to repeat the measurement.
-        number: The times number to execute the function.
+    :param func: The function to be measured.
+    :return: The timer result.
     """
-    queue.put(timer(func, *args, repeat=repeat, number=number))
+    return timer(func)
 
 
-def print_timer_result(funcs: list[Callable], *args, repeat: int = 10, number: int = 1000) -> None:
+def print_formatted_timer_results(
+        funcs: set[Function],
+        timer_wrapper: TimerWrapperFunction = default_timer_wrapper
+) -> None:
     """
-    Prints the result of timing the execution of a function.
+    Prints the formatted timer results of functions.
 
-    Args:
-        funcs: A list of functions to be measured.
-        *args: The arguments of the function.
-        repeat: The times number to repeat the measurement.
-        number: The times number to execute the function.
+    :param funcs: The functions to be measured.
+    :param timer_wrapper: The timer wrapper function.
     """
-    process = []
-    q = Queue()
+    with ProcessPoolExecutor() as executor:
+        try:
+            results = list(executor.map(timer_wrapper, funcs))
+        except Exception as e:
+            print('An error occurred while measuring the execution time.')
+            print(e)
 
-    for index, value in enumerate(funcs):
-        print(f'create Process-{index}: {value.__name__}')
-        process.append(
-            Process(target=_get_timer_result, args=(q, value, *args), kwargs={'repeat': repeat, 'number': number})
-        )
+    baseline = min(results, key=lambda x: x.time).time
 
-    for p in process:
-        p.start()
-
-    for p in process:
-        p.join()
-
-    results = [q.get() for _ in range(len(funcs))]
-    baseline = sorted(results)[0]
-
-    print('No | Function Name            | Execution Time | Rate')
+    print('## Timer Results')
+    print('No.| Function Name            | Execution Time | Rate')
     print('---|--------------------------|----------------|--------')
-    for index, (func, result) in enumerate(zip(funcs, results)):
-        print(f'{index:<2} | {func.__name__:<24} | {result:.6f} ms  | {result / baseline:.4f}')
-
-    for p in process:
-        p.close()
+    for index, result in enumerate(results):
+        print(f'{index:<2} | {result.func.__name__:<25.25}  | {result.time:<.6f} ms | {result.time / baseline:.4f}')
