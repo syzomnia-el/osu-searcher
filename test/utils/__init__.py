9218d60ce7275ef88c987e9818fb30e476851015
syzomnia-el <sagachinjufu@gmail.com>
2025-02-19 03:18:39 +0800

    refactor: refactor test.utils package for timer

--- test/utils/__init__.py
+++ test/utils/__init__.py
@@ -1,87 +1,91 @@
 # -*- coding: utf-8 -*-
 import timeit
+from collections.abc import Callable
 from concurrent.futures import ProcessPoolExecutor
-from dataclasses import dataclass
 from statistics import mean
-from typing import Callable
-
-DEFAULT_REPEAT: int = 10
-DEFAULT_NUMBER: int = 1000
+from typing import NamedTuple
 
 type Function = Callable[[], None]
-type TimerWrapperFunction = Callable[[Function], TimerResult]
+TimerResult = NamedTuple('TimerResult', [('func', Function), ('time', float)])
+type Timer = Callable[[Function], TimerResult]
+type TimerGenerator = Callable[[int, int, bool], Timer]
 
 
-@dataclass(frozen=True)
-class TimerResult:
+def generate_timer(repeat: int = 10, number: int = 1000, silent: bool = True) -> Timer:
     """
-    The timer result type.
+    Generates a timer to measure the average execution time of a function.
 
-    Attributes:
-        func: The function to be measured.
-        time: The average execution time of the function in milliseconds.
+    :param repeat: The times the measurement is repeated.
+    :param number: The times function is executed per measurement.
+    :param silent: Whether to print the measurement result.
+    :return: A timer to measure the average execution time of a function.
     """
-    func: Function
-    time: float
-
-
-def timer(func: Function, repeat: int = DEFAULT_REPEAT, number: int = DEFAULT_NUMBER,
-          silent: bool = True) -> TimerResult:
-    """
-    Measures the average execution time of a function.
-
-    :param func: The function to be measured.
-    :param repeat: The times number to repeat the measurement.
-    :param number: The times number to execute the function.
-    :param silent: Whether to print the measurement processing information.
-    :return: The average execution time of the function in milliseconds.
-    """
-    global DEFAULT_REPEAT, DEFAULT_NUMBER
-    if not callable(func):
-        raise TypeError('func must be a callable object.')
     if not isinstance(repeat, int) or repeat <= 0:
-        raise TypeError('repeat must be a positive integer.')
+        raise TypeError('`repeat` must be a positive integer.')
     if not isinstance(number, int) or number <= 0:
-        raise TypeError('number must be a positive integer.')
+        raise TypeError('`number` must be a positive integer.')
+    if not isinstance(silent, bool):
+        raise TypeError('`silent` must be a boolean.')
 
-    if not silent:
-        print(f'Measuring {func.__name__}...')
-    times = timeit.repeat(func, repeat=repeat, number=number)
-    if not silent:
-        print(f'Measurement completed of {func.__name__}, the average execution time is {mean(times):.6f} ms')
-    return TimerResult(func=func, time=mean(times))
+    def timer(func: Function) -> TimerResult:
+        """
+        Measures the average execution time of a function.
+
+        :param func: The function to be measured.
+        :return: The timer result of the function.
+        """
+        if not callable(func):
+            raise TypeError('`func` must be a no-argument callable object.')
+
+        if not silent:
+            print(f'measuring {func.__name__}...')
+        times = timeit.repeat(func, repeat=repeat, number=number)
+        mean_time = mean(times)
+        if not silent:
+            print(
+                f'measurement completed ({func.__name__}),'
+                f'the average execution time is {mean_time:.6f} ms'
+            )
+        return TimerResult(func=func, time=mean_time)
+
+    return timer
 
 
-def default_timer_wrapper(func: Function) -> TimerResult:
+def execute_timer(funcs: set[Function], /, timer: Timer = None) -> list[TimerResult]:
     """
-    The default timer wrapper function.
-
-    :param func: The function to be measured.
-    :return: The timer result.
-    """
-    return timer(func)
-
-
-def print_formatted_timer_results(
-        funcs: set[Function],
-        timer_wrapper: TimerWrapperFunction = default_timer_wrapper
-) -> None:
-    """
-    Prints the formatted timer results of functions.
+    Executes the timer for each function in parallel.
 
     :param funcs: The functions to be measured.
-    :param timer_wrapper: The timer wrapper function.
+    :param timer: The timer function to be used.
+    :return: The timer results of each function
     """
+    if not isinstance(funcs, set) or any(not callable(i) for i in funcs):
+        raise TypeError('`funcs` must be a set of no-argument callable objects.')
+    if timer is None:
+        timer = generate_timer()
+    elif not callable(timer):
+        raise TypeError('`timer` must be a callable object.')
+
     with ProcessPoolExecutor() as executor:
         try:
-            results = list(executor.map(timer_wrapper, funcs))
+            results = list(executor.map(timer, funcs))
+            return results
         except Exception as e:
-            print('An error occurred while measuring the execution time.')
+            print('an error occurred while measuring the execution time.')
             print(e)
+    return []
+
+
+def print_timer_results(results: list[TimerResult]) -> None:
+    """ Prints the timer results as a table with readable format. """
+    if not isinstance(results, list) or any(not isinstance(i, TimerResult) for i in results):
+        raise TypeError('`results` must be a list of `TimerResult` objects.')
+    if not results:
+        print('nothing to print.')
+        return
 
     results.sort(key=lambda x: x.time)
     baseline = results[0].time
-
     print('## Timer Results')
     print('No.| Function Name             | Total Cost  | Rate')
     print('---|-------------------------- |-------------|---------')
